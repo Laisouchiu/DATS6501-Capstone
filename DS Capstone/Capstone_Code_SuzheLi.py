b5bc8fcdc2dd34b159f1bfe463c62593fe7d1bb9
Laisouchiu <108690284+Laisouchiu@users.noreply.github.com>
2024-02-04 15:48:15 -0500

    Update Capstone_Code_SuzheLi.py

--- DS Capstone/Capstone_Code_SuzheLi.py
+++ DS Capstone/Capstone_Code_SuzheLi.py
@@ -9,17 +9,12 @@ import os
 
 intakes1 = pd.read_csv('AAC_Intakes.csv')
 intakes1.info()
+intakes1.head()
 
 
 #%%
 outcomes1 = pd.read_csv('AAC_Outcomes.csv')
 outcomes1.info()
-# All datasets are from 2023 to 02/04/2024
-
-#%%
-intakes1.head()
-
-# %%
 outcomes1.head()
 
 #%%
@@ -27,9 +22,11 @@ outcomes1.head()
 
 in_and_out2 = pd.read_csv('LongBeach_Intakes_Outcomes.csv')
 in_and_out2.info()
+in_and_out2.head()
 
 # %%
 in_and_out3 = pd.read_csv('Soco_Intakes_Outcomes.csv')
 in_and_out3.info()
+in_and_out3.head()
 
 # %%
