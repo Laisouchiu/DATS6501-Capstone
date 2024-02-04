e1365c0df5ceac47c6315fb3541cc7c5aa0a6d57
Laisouchiu <108690284+Laisouchiu@users.noreply.github.com>
2024-02-04 15:41:58 -0500

    Checking Datasets

--- DS Capstone/Capstone_Code_SuzheLi.py
+++ DS Capstone/Capstone_Code_SuzheLi.py
@@ -5,25 +5,31 @@ import pandas as pd
 import os
 
 # %%
+# Shelter datasets from Austin Animal Center
 
 intakes1 = pd.read_csv('AAC_Intakes.csv')
-outcomes1 = pd.read_csv('AAC_Outcomes.csv')
-
-# All datasets are from 2023 to 02/04/2024
+intakes1.info()
 
 
 #%%
-
-intakes1.info()
+outcomes1 = pd.read_csv('AAC_Outcomes.csv')
+outcomes1.info()
+# All datasets are from 2023 to 02/04/2024
 
 #%%
 intakes1.head()
-
-#%%
-outcomes1.info()
-
 
 # %%
 outcomes1.head()
 
 #%%
+# Shelter Datasets from Long Beach (For Los Angeles Area)
+
+in_and_out2 = pd.read_csv('LongBeach_Intakes_Outcomes.csv')
+in_and_out2.info()
+
+# %%
+in_and_out3 = pd.read_csv('Soco_Intakes_Outcomes.csv')
+in_and_out3.info()
+
+# %%
