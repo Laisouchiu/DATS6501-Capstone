7a585d2f5fd31da22b2914b2a8288b6f7815b1a4
Laisouchiu <108690284+Laisouchiu@users.noreply.github.com>
2024-02-05 13:00:07 -0500

    Update Capstone_Code_SuzheLi.py

--- DS Capstone/Capstone_Code_SuzheLi.py
+++ DS Capstone/Capstone_Code_SuzheLi.py
@@ -18,15 +18,23 @@ outcomes1.info()
 outcomes1.head()
 
 #%%
-# Shelter Datasets from Long Beach (For Los Angeles Area)
-
+# Shelter Datasets from Long Beach (For Los Angeles Area, South California)
 in_and_out2 = pd.read_csv('LongBeach_Intakes_Outcomes.csv')
 in_and_out2.info()
 in_and_out2.head()
 
-# %%
+#%%
+# Shelter Datasets from Sonoma County (For Bay Area, North California)
 in_and_out3 = pd.read_csv('Soco_Intakes_Outcomes.csv')
 in_and_out3.info()
 in_and_out3.head()
 
+# %%
+outcomes1.columns
+
+#%%
+in_and_out2.columns
+
+#%%
+in_and_out3.columns
 # %%
