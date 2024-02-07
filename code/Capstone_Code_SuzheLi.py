1b7a6acb6c3ec62afd035c0c74b0c2f862e5148d
Laisouchiu <108690284+Laisouchiu@users.noreply.github.com>
2024-02-07 16:35:47 -0500

    Update Capstone_Code_SuzheLi.py

--- code/Capstone_Code_SuzheLi.py
+++ code/Capstone_Code_SuzheLi.py
@@ -7,15 +7,15 @@ import os
 # %%
 # Shelter datasets from Austin Animal Center
 
-intakes1 = pd.read_csv('AAC_Intakes.csv')
-intakes1.info()
-intakes1.head()
+aac_intakes = pd.read_csv('AAC_Intakes.csv')
+aac_intakes.info()
+aac_intakes.head()
 
 
 #%%
-outcomes1 = pd.read_csv('AAC_Outcomes.csv')
-outcomes1.info()
-outcomes1.head()
+aac_outcomes = pd.read_csv('AAC_Outcomes.csv')
+aac_outcomes.info()
+aac_outcomes.head()
 
 #%%
 # Shelter Datasets from Long Beach (For Los Angeles Area, South California)
@@ -30,11 +30,60 @@ in_and_out3.info()
 in_and_out3.head()
 
 # %%
-outcomes1.columns
 
 #%%
 in_and_out2.columns
 
 #%%
 in_and_out3.columns
+
+
+#%%
+
+############################### Austin Animal Center Outcomes ######################################
+
+
+# Some EDA Example: https://www.kaggle.com/datasets/aaronschlegel/austin-animal-center-shelter-outcomes-and/code 
+
+## Features needed to be removed: Animal ID, Name, DateBirth, Outcome Subtype
+## Check unique values' count for these features: Outcome Type, Animal Type, Sex upon Outcome, Breed, Color
+## Features needed manipulations: Outcome Type, Sex Upon Outcome, Age upon Outcome
+
+#%%
+# Basic check
+aac_outcomes.info()
+aac_outcomes.head()
+
+# %%
+# Removing useless features
+remove = ['Animal ID', 'Name', 'Date of Birth', 'Outcome Subtype']
+outcomes1 = aac_outcomes.drop(remove, axis=1)
+outcomes1.info()
+
+# %%
+# Renaming features for better understanding
+new_names = {"DateTime":"Outcome DateTime", "MonthYear":"Outcome MonthYear"}
+outcomes1 = outcomes1.rename(columns=new_names)
+outcomes1.info()
+
+# %%
+# Checking unique values for some features and decide the manipulation methods
+
+unique_check1 = ['Outcome Type', 'Animal Type', 'Sex upon Outcome', 'Breed', 'Color']
+
+for feature in unique_check1:
+    print(f" '{feature}' feature has {outcomes1[feature].nunique()} unique values.")
+
+# %%
+
+## Outcome Type manipulation: 
+
+# Outcome Type feature has 11 unique values, 
+# which is more than our first though
+# So we try to check and see how to manipulate it into only 2 options
+
+outcomes1['Outcome Type'].value_counts()
+
+## Based on the check, we can of course first erased some meaningless values:
+
 # %%
